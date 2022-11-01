8d7805525448f5786794fe2279035644410ae317
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2022-10-31 22:16:15 -0300

    Add files via upload

---
+++ ManterCliente/src/main/java/com/prog4/ManterCliente/ManterClienteApplication.java
@@ -0,0 +1,13 @@
+package com.prog4.ManterCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManterClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManterClienteApplication.class, args);
+	}
+
+}
---
+++ ManterCliente/src/test/java/com/prog4/ManterCliente/ManterClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog4.ManterCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManterClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
